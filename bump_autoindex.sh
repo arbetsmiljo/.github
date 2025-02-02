6fed8e3ba9a528e2b9a7a8cf36f87238a29646d6
Henry Catalini Smith <henry@catalinismith.com>
2025-02-02 21:12:29 +0100

    Add bump shell script

---
+++ bump_autoindex.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+cd autoindex
+
+highest_tag=$(git tag -l | grep -E 'v[0-9]+' | sort -V | tail -n 1)
+echo "Highest tag: ${highest_tag}"
+
+cd ..
+
+for dir in ./20*/ ./year; do
+  if [ -d "$dir" ]; then
+    echo "$dir"
+    find "$dir" -type f -exec sh -c 'LC_CTYPE=C sed -i "" -E "s/arbetsmiljo\/autoindex@v[0-9]+/arbetsmiljo\/autoindex@${1}/g" "$2"' _ "$highest_tag" {} \;
+
+    cd "$dir"
+    if git diff --quiet; then
+      echo "No changes in $dir."
+    else
+      git --no-pager diff
+      git commit -am "Bump autoindex to ${highest_tag}"
+      git push
+    fi
+    cd ..
+  fi
+done
+
